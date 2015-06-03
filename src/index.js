c898db176333deb15e5144126f83a8aa02313d14
Tamara Cook <Tamara.Cook@icloud.com>
2015-06-03 13:10:01 +0200

    * set publicPath in webpack for serving statics in nginx * load libraries as minified with script loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // libraries
 import angular from 'angular';
-import 'angular-ui-router';
-import 'angular-ui-router.statehelper';
-import 'script!../bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js';
-import '../bower_components/angular-breadcrumb/release/angular-breadcrumb';
+import 'script!angular-ui-router';
+import 'script!angular-ui-router.stateHelper';
+import 'script!angular-bootstrap';
+import 'script!angular-breadcrumb';
 import 'script!ngstorage';
-import 'oclazyload';
+import 'script!oclazyload';
 
 // config
 import {config, run} from './config.js';
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: './dist'
+    path: './dist',
+    publicPath: 'dist/'
   },
   module: {
     loaders: [
@@ -33,7 +34,12 @@ module.exports = {
   },
   resolve: {
     alias: {
-      ngstorage: __dirname+'/bower_components/ngstorage/ngStorage.min.js'
+      ngstorage: __dirname+'/bower_components/ngstorage/ngStorage.min.js',
+      'angular-ui-router': __dirname+'/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+      'angular-ui-router.stateHelper': __dirname+'/bower_components/angular-ui-router.stateHelper/statehelper.min.js',
+      'angular-bootstrap': __dirname+'/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+      'angular-breadcrumb': __dirname+'/bower_components/angular-breadcrumb/release/angular-breadcrumb.min.js',
+      oclazyload: __dirname+'/bower_components/oclazyload/dist/ocLazyLoad.min.js'
     }
   }
 };
