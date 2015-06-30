5bc34eaa3342d3f09496333ae0d06ce56864749e
Tamara Cook <Tamara.Cook@icloud.com>
2015-06-30 17:31:28 +0200

    * now vega specs are always rendered when spec changes

--- src/plot/ng-vega.js
+++ src/plot/ng-vega.js
@@ -2,31 +2,41 @@ import vg from 'vega';
 export default /*@ngInject*/function ngVegaDirective(){
   function link(scope,el){
     let view;
-    function parse(){
-      vg.parse.spec(scope.spec,function(chart){
-        view = chart({
-          el: el[0],
-          data: scope.data,
-          renderer: scope.renderer || 'svg'
+    function parse(spec){
+      if(spec){
+        vg.parse.spec(spec,function(chart){
+          const obj = {
+            el: el[0],
+            renderer: scope.renderer || 'svg'
+          };
+          if(scope.data){
+            obj.data = scope.data;
+          }
+          view = chart(obj).update();
         });
-      });
+      }
     }
+    parse(scope.spec);
     const specClean = scope.$watch('spec',parse,true);
     function updateData(data){
-      if(view){
-        view.data(data).update().render();
-      }
-      else{
-        parse();
+      if(data){
+        if(view){
+          view.data(data).update().render();
+        }
+        else{
+          parse(scope.spec);
+        }
       }
     }
     const dataClean = scope.$watch('data',updateData,true);
     function updateRenderer(renderer){
-      if(view){
-        view.renderer(renderer).update();
-      }
-      else{
-        parse();
+      if(renderer){
+        if(view){
+          view.renderer(renderer).update();
+        }
+        else{
+          parse(scope.spec);
+        }
       }
     }
     const rendererClean = scope.$watch('renderer',updateRenderer);
